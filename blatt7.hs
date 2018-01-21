be673c47cb8cb4a088f089ff71a5b9f118340d3a
Tarek Stelzle <tstelzle@online.de>
2018-01-21 17:53:25 +0100

    Uebungsblatt 7

--- blatt7.hs
+++ blatt7.hs
@@ -25,14 +25,13 @@ instance Enum Nat where
  fromEnum Zero = 0
  fromEnum (Succ x) = 1 + fromEnum x
 
---Aufgabe 1d) NICHT GELOEST
-{-
+--Aufgabe 1d)
 instance Show Nat where
- show Zero = "0" 
- show (Succ x) = (help x y) where
-  help Zero b = "x"
-  help (Succ x) a = help x (a+1)
--}
+ show a = shows (help2 a) "" 
+
+help2 :: Nat -> Int
+help2 (Succ b) = 1 + (help2 b)
+help2 Zero = 0
 
 --Aufgabe 1e)
 instance Num Nat where
@@ -67,19 +66,38 @@ makeNat x y = Succ $ makeNat (x-1) y
 solutions :: [(Nat, Nat, Nat)]
 solutions = [ (toEnum a,toEnum b,toEnum c) | c <- [0..], b <- [0..c^2] , a <- [0..c^2], 2*a^3 + 5*b + 2 == c^2]
 
---Aufgabe 2
+--Aufgabe 2 NICHT GELOEST
 data STree a = BinS (STree a) a (STree a) | LeftS (STree a) a | RightS a (STree a) | LeafS a
 
 {-
 instance Show (STree a) where
- show (LeafS a) = a
- show (LeftS x y) = y(show x)
- show (RightS x y) = x(show y)
- show (BinS x y z) = y(show x, show z)
+ show (LeafS a) = shows a ""
+ show (LeftS x y) = undefined
+ show (RightS x y) = undefined
+ show (BinS x y z) = undefined
 -}
+--Aufgabe 3
+data Tree a = V a | F a [Tree a] deriving Show
 
-treeAnd :: STree Bool -> Bool
-treeAnd (LeafS a) = if(a == True) then True else False
-treeAnd (RightS a x) = and a (treeAnd x)
-treeAnd (LeftS a x) = and (treeAnd a) x
-treeAnd (BinS x y z) = and (treeAnd x) $ and y (treeAnd z) 
+baum = F True [V True, F True [V True, V False]]
+baum2 = F False [V True, V False]
+
+--Aufgabe 3a)
+treeAnd :: Tree Bool -> Bool
+treeAnd (V a) = if (a == True) then True else False
+treeAnd (F a xs) = if (a == False) then False else and $ map treeAnd xs
+
+--Aufgabe 3b)
+treeZip :: Tree a -> Tree b -> Tree (a,b)
+treeZip (V a) (V b) = (V (a,b))
+treeZip (V a) (F b bs) = (V (a,b))
+treeZip (F a as) (V b) = (V (a,b))
+treeZip (F a as) (F b bs) = (F (a,b) (help as bs))
+
+help :: [Tree a] -> [Tree b] -> [Tree (a,b)]
+help [] _ = []
+help _ [] = []
+help ((V a):as) ((V b):bs) = (V (a,b)) : (help as bs) 
+help ((F a ass):as) ((V b):bs) = (V (a,b)) : (help as bs)
+help ((V a):as) ((F b bss):bs) = (V (a,b)): (help as bs)
+help ((F a ass):as) ((F b bss):bs) = (F (a,b) (help ass bss)) : (help as bs)
